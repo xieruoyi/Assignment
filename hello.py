5cc8d882eca562b3e3d86776be13ce77a3388142
Ruoyi <ruoyi.xie@mail.utoronto.ca>
2023-09-23 11:19:15 -0400

    Activity 4 Finish step 1&2

--- hello.py
+++ hello.py
@@ -1,13 +1,26 @@
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+# class EmailForm(FlaskForm):
+#     email = StringField('What is your UofT email address?', validators=[DataRequired(), Email()])
+#     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,12 +32,13 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
